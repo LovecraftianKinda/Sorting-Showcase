ddefdc957991e45709b85c8f18d1246ff1530ed7
Akshansh <thebigolwiseone@gmail.com>
2025-01-30 23:13:45 +0530

    real progress?

--- sorting.c
+++ sorting.c
@@ -3,8 +3,11 @@
 #define dll __declspec(dllexport)
 
 int main(){
-
     return 0;
+}
+
+dll void free_memory(int* array) {
+    free(array);
 }
 
 dll int* bubble_sort(int array[],int size){
@@ -18,9 +21,31 @@ dll int* bubble_sort(int array[],int size){
                 array[j] = array[j+1];
                 array[j+1] = temp;
             }
-
         }
-
     }
     return array;
+}
+
+dll int* insertion_sort(int array[],int size){
+
+    int i,j,temp;
+
+    for (i = 1 ; i < size ; i++ ){
+        for (j = i; j > 0 ; j--){
+            if (array[j] < array[j-1]) {
+                temp = array[j];
+                array[j] = array[j-1];
+                array[j-1] = temp;
+            } else{
+                break;
+            }
+        }
+    }
+    return array;
+}
+
+
+dll int* selection_sort(int array[], int size){
+    
+    return 0;
 }
--- sorting.py
+++ sorting.py
@@ -2,3 +2,32 @@ import ctypes
 import os
 import random
 import time
+
+dll = ctypes.CDLL(os.path.abspath("sorting.dll"))
+
+dll.bubble_sort.argtypes = [ctypes.POINTER(ctypes.c_int), ctypes.c_int]
+dll.bubble_sort.restype = ctypes.POINTER(ctypes.c_int)
+
+dll.free_memory.argtypes = [ctypes.POINTER(ctypes.c_int)]
+dll.free_memory.restype = None
+
+#this is temporary
+n = int(input("enter number :"))
+
+array = [random.randint(0, n) for i in range(n)]
+
+
+c_array = (ctypes.c_int * len(array))(*array)
+
+time_start = time.time()
+sorted_array_ptr = dll.bubble_sort(c_array, len(array))
+
+time_end = abs(time.time() - time_start)
+
+sorted_array = [sorted_array_ptr[i] for i in range(len(array))]
+
+print(f"Time taken: {time_end}s")
+
+
+
+dll.free_memory(sorted_array_ptr)
