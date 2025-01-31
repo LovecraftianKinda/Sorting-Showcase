668843f68e80d4cc3188bd25d33f762a2f97b94e
Akshansh <thebigolwiseone@gmail.com>
2025-01-31 20:25:30 +0530

    added insertion sort

--- sorting.c
+++ sorting.c
@@ -47,5 +47,21 @@ dll int* insertion_sort(int array[],int size){
 
 dll int* selection_sort(int array[], int size){
     
+    int i , min , j , temp;
+
+    for (i=0 ; i < size ; i++){
+
+        min = i;
+
+        for (j = i+1 ; j < size ; j++){
+            if (array[j] < array[min]){
+                min = j;
+            }
+        }
+
+        temp = array[i];
+        array[i] = array[min];
+        array[min] = temp;
+    }
     return array;
 }
