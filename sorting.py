23630a844b426d4bba374f19f6cc1cda00399450
Akshansh <thebigolwiseone@gmail.com>
2025-01-31 21:06:04 +0530

    cleanup again and fixed a bug where the function template was printing function address instead of function __name__

--- sorting.py
+++ sorting.py
@@ -5,30 +5,25 @@ import time
 
 dll = ctypes.CDLL(os.path.abspath("sorting.dll"))
 
-dll.bubble_sort.argtypes = [ctypes.POINTER(ctypes.c_int), ctypes.c_int]
-dll.bubble_sort.restype = ctypes.POINTER(ctypes.c_int)
-
-dll.selection_sort.argtypes = [ctypes.POINTER(ctypes.c_int), ctypes.c_int]
-dll.selection_sort.restype = ctypes.POINTER(ctypes.c_int)
-
-dll.insertion_sort.argtypes = [ctypes.POINTER(ctypes.c_int), ctypes.c_int]
-dll.insertion_sort.restype = ctypes.POINTER(ctypes.c_int)
-
 dll.free_memory.argtypes = [ctypes.POINTER(ctypes.c_int)]
 dll.free_memory.restype = None
 
-n = int(input("enter number :"))
-
-array = [random.randint(0, n) for i in range(n)]
 
 def template(func,array):
+    
+    func.argtypes = [ctypes.POINTER(ctypes.c_int), ctypes.c_int]
+    func.restype = ctypes.POINTER(ctypes.c_int)
     c_array = (ctypes.c_int * len(array))(*array)
     time_start = time.time()
     sorted_array_ptr = func(c_array, len(array))
     time_end = abs(time.time() - time_start)
     sorted_array = [sorted_array_ptr[i] for i in range(len(array))]
-    print(f"Time taken: {time_end}s in {func} and the sorted array is {sorted_array}")
+    print(f"Time taken: {time_end}s in {func.__name__} and the sorted array is {sorted_array}")
     return sorted_array,time_end
+
+n = int(input("enter number :"))
+
+array = [random.randint(0, n) for i in range(n)]
 
 bubble_sort_array,bubble_sort_time = template(dll.bubble_sort,array)
 selection_sort_array,selection_sort_time = template(dll.selection_sort,array)
