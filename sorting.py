5956908c596bb3169aa825acad3d8d5ff02d1d5e
Akshansh <thebigolwiseone@gmail.com>
2025-01-31 20:24:59 +0530

    cleaner implementation

--- sorting.py
+++ sorting.py
@@ -17,26 +17,19 @@ dll.insertion_sort.restype = ctypes.POINTER(ctypes.c_int)
 dll.free_memory.argtypes = [ctypes.POINTER(ctypes.c_int)]
 dll.free_memory.restype = None
 
-#this is temporary
 n = int(input("enter number :"))
 
 array = [random.randint(0, n) for i in range(n)]
 
+def template(func,array):
+    c_array = (ctypes.c_int * len(array))(*array)
+    time_start = time.time()
+    sorted_array_ptr = func(c_array, len(array))
+    time_end = abs(time.time() - time_start)
+    sorted_array = [sorted_array_ptr[i] for i in range(len(array))]
+    print(f"Time taken: {time_end}s in {func} and the sorted array is {sorted_array}")
+    return sorted_array,time_end
 
-c_array = (ctypes.c_int * len(array))(*array)
-time_start = time.time()
-sorted_array_ptr = dll.bubble_sort(c_array, len(array))
-time_end = abs(time.time() - time_start)
-sorted_array = [sorted_array_ptr[i] for i in range(len(array))]
-print(f"Time taken: {time_end}s in bubble sort")
-
-
-c_array = (ctypes.c_int * len(array))(*array)
-time_start = time.time()
-sorted_array_ptr = dll.insertion_sort(c_array, len(array))
-time_end = abs(time.time() - time_start)
-sorted_array = [sorted_array_ptr[i] for i in range(len(array))]
-print(f"Time taken: {time_end}s in insertion sort")
-
-
-dll.free_memory(sorted_array_ptr)
+bubble_sort_array,bubble_sort_time = template(dll.bubble_sort,array)
+selection_sort_array,selection_sort_time = template(dll.selection_sort,array)
+insertion_sort_array,insertion_sort_time = template(dll.insertion_sort,array)
