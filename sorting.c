55511895ef329064cc0c84d67135a291a0b42839
Akshansh <thebigolwiseone@gmail.com>
2025-02-01 19:40:54 +0530

    formatted

--- sorting.c
+++ sorting.c
@@ -2,35 +2,43 @@
 #include <stdlib.h>
 #define dll __declspec(dllexport)
 
-dll void free_array(int* array){
+dll void free_array(int *array)
+{
     free(array);
 }
 
-dll int* bubble_sort(int array[],int size);
-dll int* insertion_sort(int array[],int size);
-dll int* selection_sort(int array[],int size);
-dll int* merge(int left_array[], int right_array[], int left_size, int right_size);
-dll int* merge_sort(int array[], int size);
+dll int *bubble_sort(int array[], int size);
+dll int *insertion_sort(int array[], int size);
+dll int *selection_sort(int array[], int size);
+dll int *merge(int left_array[], int right_array[], int left_size, int right_size);
+dll int *merge_sort(int array[], int size);
 
-int main(){
+int main()
+{
 
     return 0;
 }
 
-dll int* bubble_sort(int array[], int size) {
+dll int *bubble_sort(int array[], int size)
+{
     // Time complexity: O(n^2)
-    int* sorted_array = (int*)malloc(size * sizeof(int));
-    for (int i = 0; i < size; i++) {
+    int *sorted_array = (int *)malloc(size * sizeof(int));
+    for (int i = 0; i < size; i++)
+    {
         sorted_array[i] = array[i];
     }
     int temp, flag = 1;
-    for (int i = 0; i < size; i++) {
-        if (flag == 0) {
+    for (int i = 0; i < size; i++)
+    {
+        if (flag == 0)
+        {
             break;
         }
         flag = 0;
-        for (int j = 0; j < size - 1; j++) {
-            if (sorted_array[j] > sorted_array[j + 1]) {
+        for (int j = 0; j < size - 1; j++)
+        {
+            if (sorted_array[j] > sorted_array[j + 1])
+            {
                 temp = sorted_array[j];
                 sorted_array[j] = sorted_array[j + 1];
                 sorted_array[j + 1] = temp;
@@ -41,20 +49,27 @@ dll int* bubble_sort(int array[], int size) {
     return sorted_array;
 }
 
-dll int* insertion_sort(int array[], int size) {
+dll int *insertion_sort(int array[], int size)
+{
     // Time complexity: O(n^2)
-    int* sorted_array = (int*)malloc(size * sizeof(int));
-    for (int i = 0; i < size; i++) {
+    int *sorted_array = (int *)malloc(size * sizeof(int));
+    for (int i = 0; i < size; i++)
+    {
         sorted_array[i] = array[i];
     }
     int temp;
-    for (int i = 1; i < size; i++) {
-        for (int j = i; j > 0; j--) {
-            if (sorted_array[j] < sorted_array[j - 1]) {
+    for (int i = 1; i < size; i++)
+    {
+        for (int j = i; j > 0; j--)
+        {
+            if (sorted_array[j] < sorted_array[j - 1])
+            {
                 temp = sorted_array[j];
                 sorted_array[j] = sorted_array[j - 1];
                 sorted_array[j - 1] = temp;
-            } else {
+            }
+            else
+            {
                 break;
             }
         }
@@ -62,21 +77,27 @@ dll int* insertion_sort(int array[], int size) {
     return sorted_array;
 }
 
-dll int* selection_sort(int array[], int size) {
+dll int *selection_sort(int array[], int size)
+{
     // Time complexity: O(n^2)
-    int* sorted_array = (int*)malloc(size * sizeof(int));
-    for (int i = 0; i < size; i++) {
+    int *sorted_array = (int *)malloc(size * sizeof(int));
+    for (int i = 0; i < size; i++)
+    {
         sorted_array[i] = array[i];
     }
     int min, temp;
-    for (int i = 0; i < size; i++) {
+    for (int i = 0; i < size; i++)
+    {
         min = i;
-        for (int j = i + 1; j < size; j++) {
-            if (sorted_array[j] < sorted_array[min]) {
+        for (int j = i + 1; j < size; j++)
+        {
+            if (sorted_array[j] < sorted_array[min])
+            {
                 min = j;
             }
         }
-        if (min != i) {
+        if (min != i)
+        {
             temp = sorted_array[i];
             sorted_array[i] = sorted_array[min];
             sorted_array[min] = temp;
@@ -85,57 +106,70 @@ dll int* selection_sort(int array[], int size) {
     return sorted_array;
 }
 
-int* merge(int left_array[], int right_array[], int left_size, int right_size){
-    int* sorted_array = (int* )malloc(sizeof(int) * (left_size+right_size));
+int *merge(int left_array[], int right_array[], int left_size, int right_size)
+{
+    int *sorted_array = (int *)malloc(sizeof(int) * (left_size + right_size));
     int i = 0, j = 0, k = 0;
-    while (i<left_size && j<right_size){
-        if (left_array[i] <= right_array[j]) {
+    while (i < left_size && j < right_size)
+    {
+        if (left_array[i] <= right_array[j])
+        {
             sorted_array[k] = left_array[i];
             i++;
-        } else {
+        }
+        else
+        {
             sorted_array[k] = right_array[j];
             j++;
         }
         k++;
     }
-    for (; i < left_size; i++){
+    for (; i < left_size; i++)
+    {
         sorted_array[k] = left_array[i];
         k++;
     }
-    for (; j < right_size; j++){
+    for (; j < right_size; j++)
+    {
         sorted_array[k] = right_array[j];
         k++;
     }
     return sorted_array;
 }
 
-dll int* merge_sort(int array[], int size){
+dll int *merge_sort(int array[], int size)
+{
     // Time complexity: O(nlogn)
-    if (size>1){
-        int left_size = size/2;
+    if (size > 1)
+    {
+        int left_size = size / 2;
         int right_size = size - left_size;
-        int* left_array = (int*)malloc(left_size * sizeof(int));
-        int* right_array = (int*)malloc(right_size * sizeof(int));
+        int *left_array = (int *)malloc(left_size * sizeof(int));
+        int *right_array = (int *)malloc(right_size * sizeof(int));
 
-        for (int i = 0; i < left_size; i++){
+        for (int i = 0; i < left_size; i++)
+        {
             left_array[i] = array[i];
-            }
-        for (int i = 0; i < right_size; i++){
+        }
+        for (int i = 0; i < right_size; i++)
+        {
             right_array[i] = array[left_size + i];
         }
 
-        int* sorted_left = merge_sort(left_array,left_size);
-        int* sorted_right = merge_sort(right_array,right_size);
+        int *sorted_left = merge_sort(left_array, left_size);
+        int *sorted_right = merge_sort(right_array, right_size);
 
-        int* merged_array = merge(sorted_left,sorted_right,left_size,right_size);
-        
+        int *merged_array = merge(sorted_left, sorted_right, left_size, right_size);
+
         free(left_array);
         free(right_array);
-        
-        if (left_size > 1) {
+
+        if (left_size > 1)
+        {
             free(sorted_left);
         }
-        if (right_size > 1) {
+        if (right_size > 1)
+        {
             free(sorted_right);
         }
 
--- sorting.py
+++ sorting.py
@@ -7,8 +7,9 @@ dll = ctypes.CDLL(os.path.abspath("sorting.dll"))
 dll.free_array.argtypes = [ctypes.POINTER(ctypes.c_int)]
 dll.free_array.restype = None
 
-def template(func,array):
-    
+
+def template(func, array):
+
     func.argtypes = [ctypes.POINTER(ctypes.c_int), ctypes.c_int]
     func.restype = ctypes.POINTER(ctypes.c_int)
     c_array = (ctypes.c_int * len(array))(*array)
@@ -17,16 +18,17 @@ def template(func,array):
     time_end = abs(time.time() - time_start)
     sorted_array = [sorted_array_ptr[i] for i in range(len(array))]
     print(f"Time taken: {time_end}s in {func.__name__}")
-    
+
     dll.free_array(sorted_array_ptr)
-    
-    return sorted_array,time_end
+
+    return sorted_array, time_end
+
 
 n = int(input("enter number :"))
 
 array = [random.randint(0, n) for i in range(n)]
 
-bubble_sort_array,bubble_sort_time = template(dll.bubble_sort,array)
-selection_sort_array,selection_sort_time = template(dll.selection_sort,array)
-insertion_sort_array,insertion_sort_time = template(dll.insertion_sort,array)
-merge_sort_array,merge_sort_time = template(dll.merge_sort,array)
+bubble_sort_array, bubble_sort_time = template(dll.bubble_sort, array)
+selection_sort_array, selection_sort_time = template(dll.selection_sort, array)
+insertion_sort_array, insertion_sort_time = template(dll.insertion_sort, array)
+merge_sort_array, merge_sort_time = template(dll.merge_sort, array)
